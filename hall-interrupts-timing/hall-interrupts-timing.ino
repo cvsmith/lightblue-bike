db185dc5f384afa15e9f9f573bd2898b7b309a4f
cvsmith <cvsmith@andrew.cmu.edu>
2016-01-14 00:04:22 -0500

    Add distance meter to bottom

--- hall-interrupts-timing/hall-interrupts-timing.ino
+++ hall-interrupts-timing/hall-interrupts-timing.ino
@@ -12,17 +12,27 @@
 #define SS 13
 #define HALL 7
 
-Adafruit_SharpMem display(SCK, MOSI, SS);
-
 #define BLACK 0
 #define WHITE 1
 
 #define BIG_NUM_SIZE 7
-#define LABEL_SIZE 2
-//#define hallState
+#define UNIT_SIZE 2
+
+#define SAMPLE_INTERVAL 2000
+#define PULSES_PER_S_TO_MPH 4.85
+#define PULSES_TO_MILES 0.00135 
+#define PREV_LENGTH 5
+
+Adafruit_SharpMem display(SCK, MOSI, SS);
 
 volatile int count = 0;
-
+volatile int total_count = 0;
+float distance = 0;
+unsigned long start = 0;
+unsigned long now;
+float spd = 0;
+int spd_round = 0;
+int prev [PREV_LENGTH]; 
 
 void setup(void) 
 {
@@ -36,29 +46,46 @@ void setup(void)
   attachInterrupt(digitalPinToInterrupt(HALL), incr, RISING);
 }
 
-int hallState;
-
 void loop(void) 
 {
-  hallState = digitalRead(HALL);
-  display.clearDisplay();
-
+  int hallCount = count;
+  // Speed stuff
+  now = millis();
+  if (start == 0) {
+    start = millis();
+  } else if (now - start >= SAMPLE_INTERVAL) {
+    Serial.print("count: "); Serial.print(hallCount);
+    spd = ((spd * 0.5) + (hallCount * 1000 * PULSES_PER_S_TO_MPH / SAMPLE_INTERVAL)) / 2;
+    Serial.print(" spd: "); Serial.println(spd);
+    count = 0;
+    start = millis();
+  }
+  
+  spd_round = round(spd);
+  distance = total_count * PULSES_TO_MILES;
+  
+  // Display stuff
   display.clearDisplay();
   // Draw big number
   display.setTextSize(BIG_NUM_SIZE);
-  if (count < 10) display.setCursor(30, 4);
+  if (spd_round < 10) display.setCursor(30, 4);
   else display.setCursor(10, 4);
-  display.println(count, DEC);
+  display.println(spd_round, DEC);
   
   // Draw little label
-  display.setTextSize(LABEL_SIZE);
+  display.setTextSize(UNIT_SIZE);
   display.println("    MPH");
-  display.refresh();
   
-  delay(500);
+  // Draw distance
+  display.setTextSize(UNIT_SIZE);
+  display.print(" "); display.print(distance, 2); display.print(" MI");
+  display.refresh();
+
+  delay(SAMPLE_INTERVAL);
 }
 
 void incr()
 {
   count++;
+  total_count++;
 }
